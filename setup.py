d175d3c964203f00263488748a2231536d679744
KaiLuZheng <lumen_o@126.com>
2018-09-16 17:26:17 +0800

    updata setup file

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+import re
+
 try:
     import flask
     print('import flask ok!')
@@ -11,7 +13,9 @@ try:
     print('import BeautifulSoup ok!')
 
 except Exception as e:
-
     print(e)
+
+    if re.search(r'No module', str(e)) is not None:
+        print('you may try: pip install %s'%(re.search(r'\'(.*?)\'').group(1))) 
 
 
