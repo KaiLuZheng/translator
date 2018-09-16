80b6bc799cd711b6b76f06ef083bb18ccd2e57e9
KaiLuZheng <lumen_o@126.com>
2018-09-16 17:33:14 +0800

    updata setup file

--- setup.py
+++ setup.py
@@ -19,3 +19,17 @@ except Exception as e:
         print('you may try: pip install %s'%(re.search(r'\'(.*?)\'', str(e)).group(1))) 
 
 
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+try :
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    opener = webdriver.Chrome(chrome_options = chrome_options)
+except Exception as e:
+    print(e)
+finally:
+    opener.close()
+    opener.quit()
+    
